a7c8247dfabd8987330a271d3cb88956ae9290d4
Stepanov Denis <kabus2213@gmail.com>
2023-06-29 18:39:29 +0300

    HW1

---
+++ HW1/script.js
@@ -0,0 +1,21 @@
+// const res1 = String(true);
+// // const res1 = String(1000);
+// // const res1 = String(null);
+// // const res1 = String(undefined);
+// console.log (res1)
+
+// const res2 = Number(123a);
+// const res2 = Number(1000);
+// const res2 = Number(undefined);
+// const res2 = Number(null);
+// const res2 = Number(false);
+// console.log (res2)
+
+// const res3 = Boolean(null);
+// const res3 = Boolean(undefined);
+// const res3 = Boolean(“”);
+// const res3 = Boolean(0);
+// const res3 = Boolean(“ “);
+// const res3 = Boolean(1000);
+const res3 = Boolean(“1200”);
+console.log (res3)
