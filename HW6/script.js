bcc593e8290e2aacb24ae646e03bf5e6d2ab4e0d
Stepanov Denis <kabus2213@gmail.com>
2023-07-03 22:17:47 +0300

    hw6

--- HW6/script.js
+++ HW6/script.js
@@ -29,21 +29,21 @@
 
 // ---FUNCTION 3---
 
-let some = prompt ('введите любое цифровое или буквенное значение')
-let some1 = prompt ('Укажите максимальную длинну строки числом')
+// let some = prompt ('введите любое цифровое или буквенное значение')
+// let some1 = prompt ('Укажите максимальную длинну строки числом')
 
-function sliceString () {
- let result = some.length
-let result1 = Number(some1)
-if (result > result1) {
-    let res = (some.slice(0, result1) + '…')
-    console.log (res)
-   }
-   else {
-    console.log (some)
-   }
-}
-sliceString (some,some1)
+// function sliceString () {
+//  let result = some.length
+// let result1 = Number(some1)
+// if (result > result1) {
+//     let res = (some.slice(0, result1) + '…')
+//     console.log (res)
+//    }
+//    else {
+//     console.log (some)
+//    }
+// }
+// sliceString (some,some1)
 
 // ---FUNCTION 4---
 
