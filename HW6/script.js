4cd1e6ee617c950551e71a93f127f62118c3e8e8
Stepanov Denis <kabus2213@gmail.com>
2023-07-03 22:17:14 +0300

    hw6

---
+++ HW6/script.js
@@ -0,0 +1,61 @@
+// ---FUNCTION 1---
+
+// let a = prompt ('Enter the first number')
+// let b = prompt ('Enter the second number')
+
+// function maxNumber (prompts) {
+//  if (a > b) {
+//     console.log (a)
+//  } else {
+//     console.log (b)
+//  }
+// }
+// maxNumber(a,b)
+
+// ---FUNCTION 2---
+
+// let a = prompt ('Enter your age')
+
+// function checkAge () {
+//  if (a < 18) {
+//     a = Boolean(false)
+//     console.log (a)
+//  } else {
+//     a = Boolean(true)
+//     console.log (a)
+//  }
+// }
+// checkAge(a)
+
+// ---FUNCTION 3---
+
+let some = prompt ('введите любое цифровое или буквенное значение')
+let some1 = prompt ('Укажите максимальную длинну строки числом')
+
+function sliceString () {
+ let result = some.length
+let result1 = Number(some1)
+if (result > result1) {
+    let res = (some.slice(0, result1) + '…')
+    console.log (res)
+   }
+   else {
+    console.log (some)
+   }
+}
+sliceString (some,some1)
+
+// ---FUNCTION 4---
+
+// let str = prompt ('Enter the word')
+// let res = str.toLowerCase ()
+// function checkWord () {
+//  if (res==='word') {
+//     let res = Boolean(true)
+//     console.log (res)
+//  } else {
+//     let res = Boolean(false)
+//     console.log (res)
+//  }
+// }
+// checkWord(str,'word')
