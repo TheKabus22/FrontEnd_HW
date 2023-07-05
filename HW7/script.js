ac52399b5cc80e377a977d8cb1e73ab6a4d1fad8
Stepanov Denis <kabus2213@gmail.com>
2023-07-05 22:13:47 +0300

    hw7

---
+++ HW7/script.js
@@ -0,0 +1,49 @@
+// ---FUNCTION 1---
+
+// let one = prompt('Enter the first number')
+// let two = prompt('Enter the second number')
+
+// const maxNumber = (a, b) => {
+//     return a > b
+// }
+
+// const result = maxNumber(one, two)
+// console.log(result)
+
+// ---FUNCTION 2---
+
+// let a = prompt('Enter your age')
+// const checkAge = age => {
+//     return age > 18
+// }
+// const result = checkAge(a)
+// console.log(result)
+
+// ---FUNCTION 3---
+
+// let some = prompt ('введите любое цифровое или буквенное значение')
+// let some1 = prompt ('Укажите максимальную длинну строки числом')
+
+// const sliceString =  (one, two) => {
+//  let result = one.length
+// let result1 = Number(two)
+// if (result > result1) {
+//     let res = (some.slice(0, result1) + '…')
+//     return res
+//    }
+//    else {
+//     return some
+//    }
+// }
+// const show = sliceString (some,some1)
+// console.log (show)
+
+// ---FUNCTION 4---
+
+let str = prompt('Enter the word')
+let res = str.toLowerCase()
+const checkWord = a => {
+    return a === 'word'
+}
+let result = checkWord(res)
+console.log(result)
