3bc077553854de30451ad00a7de5d18026f09650
Stepanov Denis <kabus2213@gmail.com>
2023-07-20 20:17:54 +0300

    HW11

--- HW11/script.js
+++ HW11/script.js
@@ -1,8 +1,8 @@
-const array = [
+const array1 = [
     { key: 1, value: "a" },
     { key: 2, value: "b" },
     { key: 3, value: "c" }
 ]
 
-const result = array.map(({ key, value }) => ({ [key]: value }));
-console.log(result)
+const result = array1.map(({ key, value }) => ({ [key]: value }));
+console.log(result) 
