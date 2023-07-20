53095981d5a124ab90f7a8979ea3c75bac7f409c
Stepanov Denis <kabus2213@gmail.com>
2023-07-20 19:47:41 +0300

    HW10

---
+++ HW10/script.js
@@ -0,0 +1,9 @@
+const findAverage = [10,45,75,22,23];
+const findAverage1 = [];
+const result = findAverage.reduce((a,b) => a+b)
+const res = result / findAverage.length
+
+const result1 = findAverage1.includes(Number)
+const res1 = Number(result1)
+console.log (res)
+console.log (res1)
