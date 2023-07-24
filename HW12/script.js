4a1e0a91867f9bed0c1821215c19042e0f572eda
Stepanov Denis <kabus2213@gmail.com>
2023-07-24 18:13:14 +0300

    HW12 rework

--- HW12/script.js
+++ HW12/script.js
@@ -1,7 +1,7 @@
 const arrayBasic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
-const arrayPositive = arrayBasic.splice(0, 10)
-const arrayNegative = arrayBasic.splice(0, 5)
-const res = arrayPositive.reduce((a, b) => a + b)
+const arrayPositive = arrayBasic.filter((item) => { item > 0 })
+const arrayNegative = arrayBasic.filter((item) => {return item <= 0 })
+const res = arrayPositive.length
 const res1 = arrayNegative.reduce((a, b) => a + b)
 
 console.log(res, res1)
