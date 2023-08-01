27317bbc73bb860e428b667df922f97c29ae07b2
Stepanov Denis <kabus2213@gmail.com>
2023-08-01 17:27:42 +0300

    HW14

---
+++ HW14/script.js
@@ -0,0 +1,14 @@
+const number = prompt('Введіть число між 0 та 3', '');
+switch (number) {
+    case '0':
+        console.log('Ви ввели число 0');
+        break
+    case '1':
+        console.log('Ви ввели число 1');
+        break
+    case '2':
+        console.log('Ви ввели число 2, а може й 3');
+    case '3':
+        console.log('Ви ввели число 2, а може й 3');
+}
+// comment only for commiting
