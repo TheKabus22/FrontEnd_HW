25cf75ede96f192894c3249662d59ab356d24550
Stepanov Denis <kabus2213@gmail.com>
2023-09-04 22:58:46 +0300

    HW19

--- HW19/script.js
+++ HW19/script.js
@@ -1,7 +1,15 @@
 async function getData() {
-    let res = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = await res.json();
-    console.log(data)
+
+    try {
+        let res = await fetch('https://jsonplaceholder.typicode.con/posts')
+        console.log(res);
+        if (!res.ok) {
+            throw new Error ('Failed to request');
+        }
+        const data = await res.json();
+        console.log(data);
+
+    } catch (e) { console.log(e) };
 }
 
 getData()
