49787d1717f6328aeeca31318af4ae0d59ec6823
Stepanov Denis <kabus2213@gmail.com>
2023-11-06 14:15:38 +0200

    UseReducer HW

---
+++ React_HW8/my-app/src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import Counter from './components/Counter';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Counter />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
